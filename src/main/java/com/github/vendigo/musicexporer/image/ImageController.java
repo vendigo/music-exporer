1acd4dcbac8d2fa62b69eaa13b58d09aae56bf6d
Dmytro Marchenko <ace.vendigo@gmail.com>
2022-07-08 15:19:55 +0300

    Integration with pubsub

--- src/main/java/com/github/vendigo/musicexporer/image/ImageController.java
+++ src/main/java/com/github/vendigo/musicexporer/image/ImageController.java
@@ -1,11 +1,12 @@
 package com.github.vendigo.musicexporer.image;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
@@ -15,7 +16,10 @@ public class ImageController {
     private final ImageService imageService;
 
     @PostMapping("/image/load")
-    public void loadImage(@RequestParam("artistId") long artistId) {
-        imageService.loadAltImage(artistId);
+    public void loadImage(@RequestBody PubsubMessage message) {
+        ByteString data = message.getData();
+        String receivedData = data.toString();
+        log.info("Received: " + receivedData);
+        //imageService.loadAltImage(artistId);
     }
 }
